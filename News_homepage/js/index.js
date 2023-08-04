d7a59d0153b67487343042192d98638e962f4765
root <root@DellG55500>
2023-08-04 10:30:33 +0700

    second commit

--- News_homepage/js/index.js
+++ News_homepage/js/index.js
@@ -1,3 +1,15 @@
 $('.col-xxl-4').height($('.col-xxl-8').height());
 
-
+console.log(1);
+$('.main-text').addClass('fs-60px');
+console.log(window.innerWidth);
+setInterval(() => {
+    console.log(window.innerWidth);
+    if (screen.availWidth > 1200 && screen.availWidth <= 1444) {
+        $('.main-text').addClass('fs-74px');    
+        $('.main-text').removeClass('fs-60px');
+    } else {
+        $('.main-text').removeClass('fs-74px');
+        $('.main-text').addClass('fs-60px');
+    }
+}, 0);
